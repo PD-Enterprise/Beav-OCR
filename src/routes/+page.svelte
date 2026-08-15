71d4e1657eff534a9909c7a322817bad1906cbf5
PDgaming <produnyadehingia12345@gmail.com>
2026-08-15 12:20:45 +0530

    feat: implementing the components into home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import FileUpload from '$lib/components/fileUpload.svelte';
+	import Image from '$lib/components/image.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
+	import Output from '$lib/components/output.svelte';
+	import { image } from '$lib/store/store.svelte';
+</script>
+
+<div class="flex flex-col gap-2">
+	<Navbar />
+
+	{#if !image.value}
+		<FileUpload />
+	{:else}
+		<div class="flex flex-row gap-2 p-2">
+			<Image image={image.value} />
+			<Output />
+		</div>
+	{/if}
+</div>
